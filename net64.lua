f286dc7fd6a0015a6dc27741e2b0a15feca64fab
Jerry Liu <jerryjliu98@gmail.com>
2017-02-12 18:03:32 -0800

    added dropout to discriminator to test

--- net64.lua
+++ net64.lua
@@ -27,18 +27,22 @@ netD:add(nn.VolumetricConvolution(1,64,4,4,4,2,2,2,1,1,1))
 netD:add(nn.VolumetricBatchNormalization(64))
 netD:add(nn.LeakyReLU(opt.leakyslope, true))
 -- 64x32x32x32 -> 128x16x16x16
+--netD:add(nn.VolumetricDropout(0.2))
 netD:add(nn.VolumetricConvolution(64,128,4,4,4,2,2,2,1,1,1))
 netD:add(nn.VolumetricBatchNormalization(128))
 netD:add(nn.LeakyReLU(opt.leakyslope, true))
 -- 128x16x16x16 -> 256x8x8x8
+--netD:add(nn.VolumetricDropout(0.2))
 netD:add(nn.VolumetricConvolution(128,256,4,4,4,2,2,2,1,1,1))
 netD:add(nn.VolumetricBatchNormalization(256))
 netD:add(nn.LeakyReLU(opt.leakyslope, true))
 -- 256x8x8x8 -> 512x4x4x4
+--netD:add(nn.VolumetricDropout(0.2))
 netD:add(nn.VolumetricConvolution(256,512,4,4,4,2,2,2,1,1,1))
 netD:add(nn.VolumetricBatchNormalization(512))
 netD:add(nn.LeakyReLU(opt.leakyslope, true))
 -- 512x4x4x4 -> 1x1x1x1
+--netD:add(nn.VolumetricDropout(0.2))
 netD:add(nn.VolumetricConvolution(512,1,4,4,4))
 netD:add(nn.Sigmoid())
 netD:add(nn.View(1):setNumInputDims(4))
